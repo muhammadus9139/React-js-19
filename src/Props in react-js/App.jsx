f5ca1efae0c593dc92ad9b422de43250dc8f6eb7
Muhammad Usama <muhammadus9139@gmail.com>
2026-06-28 14:57:32 +0500

    Create App.jsx

---
+++ src/Props in react-js/App.jsx
@@ -0,0 +1,14 @@
+import Props from "./props";
+function App() {
+
+  
+    return(
+      <div>
+        <h1>Props in react</h1>
+  
+       <Props name="Usama" age={20}/>
+      </div>
+    );
+}
+
+export default App;
