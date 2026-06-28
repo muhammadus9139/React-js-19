9041038c8d1080af6abb219e6a3152bd9e99483f
Muhammad Usama <muhammadus9139@gmail.com>
2026-06-28 15:04:27 +0500

    Create App.jsx

---
+++ src/Default props/App.jsx
@@ -0,0 +1,17 @@
+import {useState} from "react";
+import User from "./User";
+
+function App() {
+
+      
+    return(
+      <div>
+      <h1>Props in React js</h1>
+      <User name="Usama"/>
+      <User/>
+      <User/>
+      </div>
+    );
+}
+
+export default App;
