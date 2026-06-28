8519a7e3684ecdcf66bbffb78d91ef31e7512f00
Muhammad Usama <muhammadus9139@gmail.com>
2026-06-28 15:00:54 +0500

    Create Props.jsx

---
+++ src/Props example 2/Props.jsx
@@ -0,0 +1,12 @@
+function Props({fname,lname,email,age}){
+    
+    return(
+        <div>
+            <h1>Props in React js</h1>
+          
+           <h1>{fname} {lname}, {age} , {email}</h1> 
+        </div>
+    );
+}
+
+export default Props;
