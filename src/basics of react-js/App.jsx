720656ea3d9d6a4373e72a0b60d5ed632cb9302b
Muhammad Usama <muhammadus9139@gmail.com>
2026-06-28 14:45:34 +0500

    Add counter functionality to App component

--- src/basics of react-js/App.jsx
+++ src/basics of react-js/App.jsx
@@ -1,1 +1,18 @@
 
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import User from './User';
+
+function App() {
+  const [counter, setCounter] = useState(0)
+
+  return <>
+    <h1>Counter Val :{counter}</h1>
+    <User />
+    <button onClick={()=>setCounter(counter+1)}>Increase Counter Val</button>
+  </>;
+}
+
+export default App;
