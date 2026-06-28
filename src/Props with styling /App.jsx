624e83a0962ab22210ce4b9ea4f00dbb36d1069c
Muhammad Usama <muhammadus9139@gmail.com>
2026-06-28 15:05:51 +0500

    Create App.jsx

---
+++ src/Props with styling /App.jsx
@@ -0,0 +1,24 @@
+import Header from "./Header";
+import ToDo from "./ToDo";
+import {useState} from "react";
+import User from "./User";
+import Wrapper from "./Wrapper";
+
+function App() {
+
+      
+    return(
+      <div>
+      <h1>Props in React js</h1>
+      <Wrapper color="orange">
+        <h1 style={{color:"greene"}}> Hi everyone</h1>
+      </Wrapper>
+      
+        <Wrapper color="blue">
+        <h1>Hi anil</h1>
+      </Wrapper>
+      </div>
+    );
+}
+
+export default App;
