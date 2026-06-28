c05f673f2ad2617f384a88a81d392e2f29370de6
Muhammad Usama <muhammadus9139@gmail.com>
2026-06-28 15:20:41 +0500

    Create Counter.jsx

---
+++ src/Problems in props without useEffect/Counter.jsx
@@ -0,0 +1,23 @@
+//problem in counter.jsx 
+// console statement called multiple times
+
+import {useState, useEffect} from "react"; 
+
+const Counter=({count})=>{
+
+    const handlecounter=()=>{
+        console.log("Handlecounter called");
+    }
+
+    useEffect(()=>{
+        handlecounter();
+    },[count])
+    
+    return(
+        <div>
+            <h1>Counter value {count}</h1>
+        </div>
+    );
+}
+
+export default Counter;
