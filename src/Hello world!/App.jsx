07546cd9900155c212d995b72815b427c91a515e
Muhammad Usama <muhammadus9139@gmail.com>
2026-06-28 14:48:54 +0500

    Create App component with Header and greeting

---
+++ src/Hello world!/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header";
+function App() {
+  return (
+    <div>
+      <Header/>
+      <h1>Hello, World!</h1>
+      <h1>new one</h1>
+    </div>
+  );
+}
+
+export default App;
