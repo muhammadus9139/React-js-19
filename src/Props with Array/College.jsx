975cc9fd82c328c8bc2994f4135d9a74f0c33a2e
Muhammad Usama <muhammadus9139@gmail.com>
2026-06-28 15:02:49 +0500

    Create College.jsx

---
+++ src/Props with Array/College.jsx
@@ -0,0 +1,13 @@
+function College({names}) { 
+    return ( 
+        <div>
+            <h1>College Component</h1>
+           <h1>Collge name: {names[0]}</h1>
+           <h1>Collge name: {names[1]}</h1>
+           <h1>Collge name: {names[2]}</h1>
+           <h1>Collge name: {names[3]}</h1>
+        </div>
+    );
+}
+
+export default College;
