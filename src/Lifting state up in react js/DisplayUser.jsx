3511b1a174f3cb956abfeb3c3cc7a62329ba821b
Muhammad Usama <muhammadus9139@gmail.com>
2026-06-29 23:06:39 +0500

    Create DisplayUser.jsx

---
+++ src/Lifting state up in react js/DisplayUser.jsx
@@ -0,0 +1,12 @@
+import { startTransition, useEffect, useState, useTransition } from "react";
+
+function DisplayUser({user}){
+    return(
+        <div>
+            <h3>Display User: {user}</h3>
+           
+        </div>
+    );
+}
+
+export default DisplayUser;
