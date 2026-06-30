e7e8197b2c2cc53f1f64c04e0ed171541aa4f7b5
Muhammad Usama <muhammadus9139@gmail.com>
2026-06-30 22:03:16 +0500

    Create College.jsx

---
+++ src/Context API/College.jsx
@@ -0,0 +1,14 @@
+import ClassComponent from "./ClassComponent";
+
+
+function College() {
+
+ return (
+    <div style={{backgroundColor:"brown", padding:"10px"}}>
+      <h1>College Component</h1>
+      <ClassComponent/>
+    </div>
+ );
+}
+
+export default College;
