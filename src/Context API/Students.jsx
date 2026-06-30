16946570207fe5bec6118994fe3ee15331b7ddb4
Muhammad Usama <muhammadus9139@gmail.com>
2026-06-30 22:04:46 +0500

    Create Students.jsx

---
+++ src/Context API/Students.jsx
@@ -0,0 +1,14 @@
+
+import Subject from "./Subject";
+
+function Students() {
+
+ return (
+    <div style={{backgroundColor:"yellow", padding:"10px"}}>
+      <h1>Student Component</h1>
+      <Subject/>
+    </div>
+ );
+}
+
+export default Students;
