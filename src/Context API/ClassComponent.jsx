39902cc7978519c957bb3aa86120e9e712ee6a48
Muhammad Usama <muhammadus9139@gmail.com>
2026-06-30 22:03:39 +0500

    Add ClassComponent with Students integration

---
+++ src/Context API/ClassComponent.jsx
@@ -0,0 +1,13 @@
+import Students from "./Students";
+
+function ClassComponent() {
+
+ return (
+    <div style={{backgroundColor:"orange", padding:"10px"}}>
+      <h1>ClassComponent</h1>
+      <Students/>
+    </div>
+ );
+}
+
+export default ClassComponent;
