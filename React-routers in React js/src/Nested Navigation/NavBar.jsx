5ffba334e141434dda4f2cf04fd1dc361e3e3f0e
Muhammad Usama <muhammadus9139@gmail.com>
2026-07-02 22:19:37 +0500

    Add NavBar component for navigation links

---
+++ React-routers in React js/src/Nested Navigation/NavBar.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import './header.css'
+
+function NavBar() {
+    return (
+        <div className="header">
+
+            <div>
+                <Link className="link" to="/"><h2>Logo</h2></Link>
+            </div>
+
+            <div>
+                <ul>
+                    <li>
+                        <Link className="link" to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link className="link" to="/Login">Login</Link>
+                    </li>
+                    <li>
+                        <Link className="link" to="/About">About</Link>
+                    </li>
+                    <li>
+                        <Link className="link" to="/college">College</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    );
+}
+
+export default NavBar
