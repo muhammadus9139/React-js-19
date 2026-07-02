4c96f24b1cf1ef01dd180665ba79dfffeb59a7c1
Muhammad Usama <muhammadus9139@gmail.com>
2026-07-02 22:38:14 +0500

    Create Users.jsx

---
+++ React-routers in React js/src/Optional segment, static and dynamic/Users.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+function Users() {
+
+
+    const usersdata = [
+        { id: 1, name: "John", email: "john@example.com" },
+        { id: 2, name: "Jane", email: "jane@example.com" },
+        { id: 3, name: "Bob", email: "bob@example.com" },
+        { id: 4, name: "Alice", email: "alice@example.com" },
+        { id: 5, name: "Mike", email: "mike@example.com" }
+    ];
+
+    return (
+
+        <div style={{ marginLeft: "20px" }}>
+            <h1>Users page</h1>
+            {
+                usersdata.map((item) => (
+                    <div >
+                        <Link to={"/users/"+item.id}>{item.name}</Link>
+                    </div>
+                ))
+            }
+        </div>
+    );
+}
+
+export default Users
