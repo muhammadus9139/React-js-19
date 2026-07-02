79ec9fc7bea0cb92638e58d240735b7f75bb03f8
Muhammad Usama <muhammadus9139@gmail.com>
2026-07-02 22:22:53 +0500

    Create App.jsx

---
+++ React-routers in React js/src/layout and index routes/App.jsx
@@ -0,0 +1,41 @@
+// import "./App.css";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import NavBar from "./NavBar";
+import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
+import PageNotFound from "./PageNotFound";
+import College from "./College";
+import CollegeDetails from "./CollegeDetails";
+import Department from "./Department";
+import Student from "./Student";  
+
+function App() {
+  return (
+    <>
+
+    <Routes> 
+
+      <Route element={<NavBar/>}>
+           <Route path="/" element={<Home/>} />
+           <Route path="/about" element={<About/>} />
+           <Route path="/login" element={<Login/>} />
+      </Route>
+
+     
+        <Route path="/college" element={<College/>}>
+            <Route path="CollegeDetails" element={<CollegeDetails/>} />
+            <Route path="Department" element={<Department/>} />
+            <Route index element={<Student/>} />
+        </Route>
+
+
+      {/* <Route path="/*" element={<PageNotFound/>} /> */}
+      {/* <Route path="/*" element={<Navigate to="/login"/>} /> */}
+    </Routes>
+
+    </>
+  );
+}
+
+export default App;
