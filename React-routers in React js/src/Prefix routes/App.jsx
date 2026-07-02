a993146085037b1cc311bf0ce6a736ae77146706
Muhammad Usama <muhammadus9139@gmail.com>
2026-07-02 22:25:55 +0500

    Create App.jsx

---
+++ React-routers in React js/src/Prefix routes/App.jsx
@@ -0,0 +1,45 @@
+// import "./App.css";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import NavBar from "./NavBar";
+import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
+import PageNotFound from "./PageNotFound";
+import College from "./College";
+import CollegeDetails from "./CollegeDetails";
+import Department from "./Department";
+import Student from "./Student";
+
+function App() {
+  return (
+    <>
+
+      <Routes>
+
+        <Route element={<NavBar />}>
+          <Route path="/" element={<Home />} />
+          
+          // this is prefix routes give path of /pak/user
+            <Route path="/pak/user">
+              <Route path="/pak/user/login" element={<Login />} />
+              <Route path="/pak/user/about" element={<About />} />
+            </Route>
+        </Route>
+
+
+        <Route path="/college" element={<College />}>
+          <Route path="CollegeDetails" element={<CollegeDetails />} />
+          <Route path="Department" element={<Department />} />
+          <Route index element={<Student />} />
+        </Route>
+
+
+        {/* <Route path="/*" element={<PageNotFound/>} /> */}
+        {/* <Route path="/*" element={<Navigate to="/login"/>} /> */}
+      </Routes>
+
+    </>
+  );
+}
+
+export default App;
