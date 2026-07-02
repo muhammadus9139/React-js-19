b1d335761cf74bc560137bc9093cf04b5302bea8
Muhammad Usama <muhammadus9139@gmail.com>
2026-07-02 22:47:04 +0500

    Create App.jsx

---
+++ src/Tailwind css in react js/App.jsx
@@ -0,0 +1,25 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      
+      <div className="bg-white p-10 rounded-2xl shadow-lg text-center">
+        
+        <h1 className="text-5xl font-bold text-red-600 mb-4">
+          Tailwind CSS in React js 
+        </h1>
+
+        <p className="text-gray-600 text-lg">
+          React + Tailwind Setup Success
+        </p>
+
+        <button className="mt-6 px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-700 transition">
+          Click Me
+        </button>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
