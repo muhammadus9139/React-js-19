aba0391bb933ba0e471dc0e5803dde0badde870d
Muhammad Usama <muhammadus9139@gmail.com>
2026-07-03 19:34:41 +0500

    Create App.jsx

---
+++ API's/Integrate API for delete method/App.jsx
@@ -0,0 +1,25 @@
+import UserAdd from "./UserAdd";
+import UserList from "./UserList";
+import { Routes, Route } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/useradd">Add user</NavLink>
+        </li>
+      </ul>
+      <h1>Integrate API for delete method</h1>
+
+      <Routes>
+        <Route path="/" element={<UserList />} />
+        <Route path="/useradd" element={<UserAdd />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
