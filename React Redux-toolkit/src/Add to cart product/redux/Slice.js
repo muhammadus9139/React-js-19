34513c63a9f3d11585044e9ff959f6dd1ca0be0f
Muhammad Usama <muhammadus9139@gmail.com>
2026-07-07 14:11:31 +0500

    Create Slice.js

---
+++ React Redux-toolkit/src/Add to cart product/redux/Slice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState={
+    items:[]
+}
+
+const addtocart=createSlice({
+    name:'cart',
+    initialState,
+    reducers:{
+       addItem:(state,action)=>{
+            // state.value += 1;
+            state.items.push(action.payload)
+       },
+       removeItem:(state)=>{
+            state.value>0 ? state.value -= 1: null;
+       },
+       clearallItem:(state)=>{
+            state.value = 0;
+       }
+    }
+})
+
+export const {addItem,removeItem,clearallItem}=addtocart.actions;
+export default addtocart.reducer;
