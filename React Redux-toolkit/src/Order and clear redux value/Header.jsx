4cec895705644d37de64dbc9631ac17dba3caa0a
Muhammad Usama <muhammadus9139@gmail.com>
2026-07-07 18:10:12 +0500

    Create Header.jsx

---
+++ React Redux-toolkit/src/Order and clear redux value/Header.jsx
@@ -0,0 +1,33 @@
+import AddCart from "./AddCart";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <div className="logo">
+        <h2>MyShop</h2>
+      </div>
+
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">
+              Home
+              <h1>Order and clear redux value</h1>
+            </Link>
+          </li>
+
+          {/* 
+          <li><a href="#">Products</a></li>
+          <li><a href="#">Categories</a></li>
+          <li><a href="#">Contact</a></li> 
+          */}
+        </ul>
+      </nav>
+
+      <AddCart />
+    </header>
+  );
+}
+
+export default Header;
