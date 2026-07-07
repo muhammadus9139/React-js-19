0ef738e38ec79096dd4334bf8c80e478557cf293
Muhammad Usama <muhammadus9139@gmail.com>
2026-07-07 18:13:03 +0500

    Create Slice.js

---
+++ React Redux-toolkit/src/Order and clear redux value/redux/Slice.js
@@ -0,0 +1,48 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  items: localStorage.getItem("cart")
+    ? JSON.parse(localStorage.getItem("cart"))
+    : [],
+};
+
+const addtocart = createSlice({
+  name: "cart",
+  initialState,
+
+  reducers: {
+    addItem: (state, action) => {
+      state.items.push(action.payload);
+
+      localStorage.setItem(
+        "cart",
+        JSON.stringify(state.items)
+      );
+    },
+
+    removeItem: (state, action) => {
+      const cartdata = state.items.filter(
+        (item) => item.id !== action.payload.id
+      );
+
+      state.items = cartdata;
+
+      localStorage.setItem(
+        "cart",
+        JSON.stringify(cartdata)
+      );
+    },
+
+    clearallItem: (state) => {
+      state.items = [];
+    },
+  },
+});
+
+export const {
+  addItem,
+  removeItem,
+  clearallItem,
+} = addtocart.actions;
+
+export default addtocart.reducer;
