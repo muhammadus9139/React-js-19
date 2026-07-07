8b6f91659b2b72b8407df651635114fb36681ece
Muhammad Usama <muhammadus9139@gmail.com>
2026-07-07 17:56:01 +0500

    Add routing and setup in App component

---
+++ React Redux-toolkit/src/Add routing and setup/App.jsx
@@ -0,0 +1,27 @@
+import { useDispatch } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import Header from "./Header";
+import Product from "./Product";
+import CartList from "./CartList";
+import { clearallItem } from "./redux/slice";
+
+function App() {
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Product />} />
+          <Route path="/cart" element={<CartList />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
