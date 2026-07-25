8860249f538f798adc0aebad59078b19554c44db
Muhammad Usama <muhammadus9139@gmail.com>
2026-07-25 16:10:06 +0500

    Create Store.js

---
+++ React Redux-toolkit/src/Call product API/redux/Store.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import cartreducer from "./slice"
+import productsreducer from "./Productslice"
+
+const store=configureStore({
+    reducer:{
+        cart:cartreducer,
+        products:productsreducer
+    }
+})
+
+export default store
